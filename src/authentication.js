7faf1df056e5918a232cd28fa2557715aec0931a
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-11-16 21:24:06 -0500

    authenticate populates hook.params.user so no need to run findUser; fixed user patch password when a temp password is used

--- src/authentication.js
+++ src/authentication.js
@@ -7,21 +7,6 @@ const FacebookStrategy = require('passport-facebook')
 const GithubStrategy = require('passport-github')
 const MyVerifier = require('./authentication-verifier')
 
-const findUser = () => hook => {
-  const userService = hook.app.service('users')
-  const email = (hook.params.user && hook.params.user.email) || hook.data.email
-  // lookup the user by email.
-  if (email) {
-    return userService.find({ query: {email} })
-      .then(({ data }) => {
-        hook.params.user = data[0]
-        return hook
-      })
-  } else {
-    return Promise.resolve(hook)
-  }
-}
-
 module.exports = function () {
   const app = this
   const config = app.get('authentication')
@@ -65,7 +50,10 @@ module.exports = function () {
       create: [
         // Return user to avoid extra request.
         // Add `tmpPasswordUsed` flag if necessary.
-        findUser(),
+        hook => {
+          console.log(`hook.params.user = `, hook.params.user)
+          return hook
+        },
         hook => {
           if (hook.params.user.tmpPasswordUsed && hook.params.user.tempPassword) {
             hook.result.tmpPasswordUsed = true
--- src/services/users/hook.check-password.js
+++ src/services/users/hook.check-password.js
@@ -1,5 +1,7 @@
 const errors = require('feathers-errors')
 const comparePassword = require('../../utils/compare-password')
+const Debug = require('debug')
+const debug = Debug('hook.check-password')
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function checkPassword (hook) {
@@ -9,6 +11,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
 
     const password = hook.data.password
     const oldPassword = hook.data.oldPassword
+    debug(`password = ${password}, oldPassword = ${oldPassword}`)
 
     // todo: figure out what to do with `emailCode`.
     // const emailCode = hook.data.emailCode
@@ -25,17 +28,24 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
       return Promise.reject(new errors.BadRequest('Please provide a password'))
     }
 
-    return comparePassword(passwordToCheck, user.password).then(() => {
-      if (hook.data.oldPassword) {
-        // delete `oldPassword` but keep `password`
-        delete hook.data.oldPassword
-      } else {
-        // in all other cases delete `password` (to not patch the existing one in db)
-        delete hook.data.password
-      }
-      return hook
-    }).catch(() => {
-      return Promise.reject(new errors.BadRequest('Incorrect password'))
-    })
+    return comparePassword(passwordToCheck, user.password)
+      .catch(() => {
+        debug(`Password failed. Trying tempPassword...`)
+        return comparePassword(passwordToCheck, user.tempPassword)
+      })
+      .then(() => {
+        debug('Password OK')
+        if (hook.data.oldPassword) {
+          // delete `oldPassword` but keep `password`
+          delete hook.data.oldPassword
+        } else {
+          // in all other cases delete `password` (to not patch the existing one in db)
+          delete hook.data.password
+        }
+        return hook
+      })
+      .catch(() => {
+        return Promise.reject(new errors.BadRequest('Incorrect password'))
+      })
   }
 }
