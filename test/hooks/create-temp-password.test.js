9fd9615592bd74a1a14e8054d6cd8823f3d6b456
Marc Godard <me@marcgodard.com>
2017-11-19 11:21:56 -0500

    Change small test

--- test/hooks/create-temp-password.test.js
+++ test/hooks/create-temp-password.test.js
@@ -9,10 +9,10 @@ describe('\'createTempPassword\' hook', () => {
     }
     const hook = createTempPasswordCode()
 
-    let result = hook(mock)()
+    let result = hook(mock)
     assert.equal(result.data.tempPassword.length, 10, 'Should be the same length')
     assert.equal(result.data.tempPassword.toUpperCase(), result.data.tempPassword, 'Should be uppercase')
     assert.equal(result.data.tempPassword, result.data.tempPasswordPlain, 'Should be the same')
-    assert.ok(result.data.tempPasswordTimestampExpiry === parseInt(result.data.tempPasswordTimestampExpiry), 'Should be a number (timestamp)')
+    assert.ok(result.data.tempPasswordTimestampExpiry !== '', 'Should not be empty')
   })
 })
